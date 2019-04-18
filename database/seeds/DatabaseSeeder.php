c44025e9b88ad95a5b72049b26d64457a80a2fae
Darwin Calva <kalvakxlvx@gmail.com>
2019-04-17 21:56:05 -0500

    Se modifico el archivo DatabaseSeeder, y ProfessionSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->Truncar_Tablas(['professions']);
         //este metodo se ejecuta al colocar en la  consola
         //php artisan DB:seed 
          $this->call(ProfessionSeeder::class);
@@ -20,5 +21,16 @@ class DatabaseSeeder extends Seeder
         //     PostsTableSeeder::class,
         //     CommentsTableSeeder::class,
         // ]);
+    }
+    public function Truncar_Tablas(array $tablas)
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');//No toma en cuenta las llaves foraneas
+        // Illuminate\Support\Facades\DB::table('users'); para no importar el modulo se puede usar este
+        foreach ($tablas as $tabla) {
+            DB::table($tabla)->truncate(); //Elimina los registros de la base de datos, 
+            //solo se puede usar cuando no se tiene restricciones de clave foranea
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');//Vuelve a tomar en cuenta las llaves foraneas
+
     }
 }
--- database/seeds/ProfessionSeeder.php
+++ database/seeds/ProfessionSeeder.php
@@ -12,11 +12,7 @@ class ProfessionSeeder extends Seeder
      */
     public function run()
     {
-    DB::statement('SET FOREIGN_KEY_CHECKS=0;');//No toma en cuenta las llaves foraneas
-       // Illuminate\Support\Facades\DB::table('users'); para no importar el modulo se puede usar este
-    DB::table('professions')->truncate(); //Elimina los registros de la base de datos, 
-    //solo se puede usar cuando no se tiene restricciones de clave foranea
-    
+        
     DB::table('professions')->insert(
           ['title'=>'Desarrollador Movil']
       );
